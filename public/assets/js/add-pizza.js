18a53d60f187f658461023bedcc397813347be66
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-03-10 19:36:20 -0600

    adjusting the bug on submit pizza

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -62,14 +62,16 @@ const handlePizzaSubmit = event => {
   })
     .then(response => response.json())
     .then(postResponse => {
+      alert('Pizza created successfully!');
       console.log(postResponse);
     })
     .catch(err => {
       console.log(err);
       saveRecord(formData);
-      // DO INDEXED DB STUFF HERE
     });
+    
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
 $addToppingBtn.addEventListener('click', handleAddTopping);
+
